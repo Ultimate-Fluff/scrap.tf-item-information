eb622765d17088cf1e66fed429d7453e93acaf20
Puckaboo2 <kianalacy@gmail.com>
2023-09-20 15:14:32 -0700

    testing

---
+++ stntrading-item-information.meta.js
@@ -0,0 +1,12 @@
+// ==UserScript==
+// @name         scrap.tf-item-information
+// @version      0.1.12
+// @description  Add Backpack.tf and Marketplace.tf links in a menu when middle clicking items on scrap.tf
+// @author       Netroscript
+// @match        https://*.stntrading.eu/*
+// @require      https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js
+// @require      https://cdnjs.cloudflare.com/ajax/libs/jquery-contextmenu/2.9.2/jquery.contextMenu.min.js
+// @downloadURL  https://github.com/NetroScript/scrap.tf-item-information/raw/master/scrap.tf-item-information.user.js
+// @updateURL    https://github.com/NetroScript/scrap.tf-item-information/raw/master/scrap.tf-item-information.meta.js
+// @run-at       document-end
+// ==/UserScript==
